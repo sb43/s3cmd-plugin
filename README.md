c81408971e2dbe309447a2660dc25e5b1c944497
garyluu <garylau.work@gmail.com>
2017-03-08 14:03:54 -0500

    Modified icgc-get-plugin and README

--- README.md
+++ README.md
@@ -6,24 +6,23 @@ Dockstore icgc file provisioning plugin
 The icgc-get plugin is capable of downloading files by calling out to an installed copy of the [icgc-get-client](http://docs.icgc.org/cloud/icgc-get).
 
 ```
-$ cat test.icgc.json
+$ cat test.json
 {
-    "input_file": {
+  "input_file": {
         "class": "File",
         "path": "icgc-get://FI509397"
     },
     "output_file": {
         "class": "File",
-        "path": "s3://oicr.temp/bamstats_report.zip"
+        "path": "/tmp/md5sum.txt"
     }
 }
 
 $ dockstore tool launch --entry  quay.io/briandoconnor/dockstore-tool-md5sum  --json test.icgc.json
 Creating directories for run of Dockstore launcher at: ./datastore//launcher-2ebce330-2a44-4a3a-9d6d-55c152a5c38e
 Provisioning your input files to your local machine
-Downloading: #input_file from icgc-get://097ddb14-9be7-5147-9d6e-7d350e7b203e into directory: /media/large_volume/dockstore_tools/dockstore-tool-md5sum/./datastore/launcher-2ebce330-2a44-4a3a-9d6d-55c152a5c38e/inputs
-/a91c615b-a8ec-452f-afef-e6da6032194d
-Calling on plugin io.dockstore.provision.ICGCGetPlugin$ICGCGetProvision to provision icgc://097ddb14-9be7-5147-9d6e-7d350e7b203e
+Downloading: #input_file from icgc-get://FI509397 into directory: /home/gluu/dockstore/dockstore-client/./datastore/launcher-423d8d83-e6b0-418d-8a09-0e29003ac55f/inputs/0aafce03-f893-4ce2-b97f-f2c36215f162
+Calling on plugin io.dockstore.provision.ICGCGetPlugin$ICGCGetProvision to provision icgc-get://FI509397
 ...
 ```
 
@@ -33,11 +32,11 @@ Calling on plugin io.dockstore.provision.ICGCGetPlugin$ICGCGetProvision to provi
 This plugin gets configuration information from the following structure in ~/.dockstore/config
 
 ```
-[icgc-get-client]
-client = /home/user/icgc-get/icgc-get
-config-file-location = /.icgc-get/config.yaml
+[dockstore-icgc-get-plugin]
+client = /home/gluu/icgc-get/icgc-get
+config-file-location = /home/gluu/.icgc-get/config.yaml
 ```
 
-Set the client location to your own and also make sure that the configuration file is populated with an access token.
+Set the client location to your own icgc-get client and also make sure that the configuration file is available at the config-file-location.
 
 
--- src/main/java/io/dockstore/provision/ICGCGetPlugin.java
+++ src/main/java/io/dockstore/provision/ICGCGetPlugin.java
@@ -15,14 +15,12 @@
  */
 package io.dockstore.provision;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
@@ -30,15 +28,12 @@ import java.util.Set;
 
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ro.fortsoft.pf4j.Extension;
 import ro.fortsoft.pf4j.Plugin;
 import ro.fortsoft.pf4j.PluginWrapper;
 import ro.fortsoft.pf4j.RuntimeMode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import sun.misc.IOUtils;
-
-import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
 
 /**
  * @author gluu
@@ -76,9 +71,22 @@ public class ICGCGetPlugin extends Plugin {
         }
 
         public Set<String> schemesHandled() {
-            return new HashSet<>(Lists.newArrayList("icgc"));
+            return new HashSet<>(Lists.newArrayList("icgc-get"));
         }
 
+        //
+        // Downloads directory will look something like:
+        // .staging
+        // {{Object ID folder}}
+        //     - logs
+        //     - {{File Name file}}
+        //
+
+        /**
+         * @param sourcePath  The scheme for icgc-get (icgc-get://FI509397)
+         * @param destination The destination where the file is supposed to be (includes the filename like /home/user/icgc-get/downloads/file.txt)
+         * @return Whether download was successful or not
+         */
         public boolean downloadFrom(String sourcePath, Path destination) {
             String client = "/icgc-get/icgc-get";
             String configLocation = "/.icgc-get/config.yaml";
@@ -93,17 +101,23 @@ public class ICGCGetPlugin extends Plugin {
 
             // ambiguous how to reference icgc-get files, rip off these kinds of headers
             String prefix = "icgc-get://";
-            if (sourcePath.startsWith(prefix)){
+            if (sourcePath.startsWith(prefix)) {
                 fileID = sourcePath.substring(prefix.length());
             }
 
             // default layout saves to original_file_name/object_id
             // file name is the directory and object id is actual file name
-            String downloadDir = destination.getParent().toFile().getAbsolutePath();
-            String bob = client + " --config " + configLocation + " download " + fileID + " --output " + destination;
+            String downloadDir = destination.getParent().toFile().getAbsolutePath() + "/tmp";
+            createDirectory(downloadDir);
+            String command = client + " --config " + configLocation + " download " + fileID + " --output " + downloadDir;
             Runtime rt = Runtime.getRuntime();
             try {
-                Process ps = rt.exec(bob);
+                Process ps = rt.exec(command);
+                try {
+                    ps.waitFor();
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
                 java.util.Scanner s = new java.util.Scanner(ps.getErrorStream()).useDelimiter("\\A");
                 String errorString = s.hasNext() ? s.next() : "";
                 System.out.println("Error String: " + errorString);
@@ -111,20 +125,68 @@ public class ICGCGetPlugin extends Plugin {
                 java.util.Scanner s2 = new java.util.Scanner(ps.getInputStream()).useDelimiter("\\A");
                 String inputString = s2.hasNext() ? s2.next() : "";
                 System.out.println("Input String: " + inputString);
-                return true;
-
             } catch (IOException e) {
                 logger.info("Could not download input file");
                 System.err.println(e.getMessage());
                 throw new RuntimeException("Could not download input file: ", e);
             }
+            moveFiles(downloadDir, destination);
+            return true;
         }
 
         public boolean uploadTo(String destPath, Path sourceFile, Optional<String> metadata) {
             throw new UnsupportedOperationException("ICGC Get does not support upload");
         }
 
-    }
+        private boolean createDirectory(String directoryPath) {
+            File theDir = new File(directoryPath);
+            boolean result = false;
+            if (!theDir.exists()) {
+                try {
+                    result = theDir.mkdir();
+                    System.out.println("Tmp directory created.");
+                } catch (SecurityException e) {
+                    System.err.println("Could not create directory due to security reasons.");
+                }
+            } else {
+                System.out.println("Tmp directory already exists.");
+            }
+            return result;
+        }
 
+        private boolean moveFiles(String downloadDir, Path destination) {
+            // List of files at the destination directory
+            String[] list = Paths.get(downloadDir).toFile().list();
+            assert list != null;
+            // File created at the destination
+            for (String folder : list) {
+                // Skip hidden directories
+                if (folder.contains(".")) {
+                    continue;
+                }
+                String s1 = downloadDir + "/" + folder;
+                Path path = Paths.get(s1);
+                String[] files = path.toFile().list();
+                assert files != null;
+                for (String file : files) {
+                    // Skip the logs directory
+                    if (file.equals("logs")) {
+                        continue;
+                    }
+                    Path downloadedFileFileObj = Paths.get(path + "/" + file);
+                    try {
+                        Files.copy(downloadedFileFileObj, destination, StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("File copied to destination.");
+                        return true;
+                    } catch (IOException ioe) {
+                        System.err.println(ioe.getMessage());
+                        throw new RuntimeException("Could not move input file: ", ioe);
+                    }
+                }
+
+            }
+            return false;
+        }
+    }
 }
 
